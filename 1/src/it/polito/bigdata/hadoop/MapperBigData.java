9151397053a16fe113e53034edb73efcd8c99ee5
RiccardoRevalor <riccardorevalor@gmail.com>
2024-10-05 13:04:44 +0200

    lab1_bonustrack

--- 1/src/it/polito/bigdata/hadoop/MapperBigData.java
+++ 1/src/it/polito/bigdata/hadoop/MapperBigData.java
@@ -23,14 +23,20 @@ class MapperBigData extends Mapper<LongWritable, // Input key type
 		// (=a space, a tab, a line break, or a form feed)
 		// The split method returns an array of strings
 		String[] words = value.toString().split("\\s+");
-
-		// Iterate over the set of words
-		for (String word : words) {
-			// Transform word case
-			String cleanedWord = word.toLowerCase();
-
-			// emit the pair (word, 1)
-			context.write(new Text(cleanedWord), new IntWritable(1));
+		
+		//2 GRAM BONUS POINTS
+		//BIGRAMS = SEQUENCE OF TWO CONSECUTIVE WORDS
+		// Iterate over the set of words and create bigrams
+		for (int i = 0; i < words.length - 1; i++) {
+			// Create the bigram
+			String biGram = words[i] + " " + words[i + 1];
+			biGram = biGram.toLowerCase();
+			
+			//cleaning: consider only alphanumeric bigrams
+			if (biGram.matches("[a-z0-9]+ [a-z0-9]+")) {
+				// Emit the bigram
+				context.write(new Text(biGram), new IntWritable(1));	
+			}
 		}
 	}
 }
